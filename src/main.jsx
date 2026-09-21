17fa3aae5422cf98c2b12e8558a3c5b3dd9ffbe7
fitrawan29 <fitrawan29@gmail.com>
2026-09-22 02:17:48 +0800

    fix: Hapus import UI.jsx yang salah di main.jsx, cleanup kode

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import './components/UI.jsx';
 import './index.css'; // This will contain Tailwind directives
 import { registerSW } from './registerSW.js';
 
